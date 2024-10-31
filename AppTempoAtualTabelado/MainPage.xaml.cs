cf35bdbfd79af67f91be9f21d8f4f84eb89d1714
Math H. Ferreira <101345181+MathFerreiraDev@users.noreply.github.com>
2024-10-31 18:57:27 -0300

    Fix: Refresh de list_view

--- AppTempoAtualTabelado/MainPage.xaml.cs
+++ AppTempoAtualTabelado/MainPage.xaml.cs
@@ -4,9 +4,8 @@ namespace AppTempoAtualTabelado
 {
     public partial class MainPage : ContentPage
     {
-
+        ObservableCollection<Produto> list_locations_cs = new ObservableCollection<Produto>();
         CancellationTokenSource _cancelTokenSource;
-        bool _isCheckingLocation;
 
         string? cidade;
         string latitude_value, longitude_value;
@@ -14,10 +13,11 @@ namespace AppTempoAtualTabelado
         public MainPage()
         {
             InitializeComponent();
+            lst_locations.ItemsSource = list_locations_cs;
         }
 
        
-
+        //OBTER AS COORDENADAS ATUAIS
         private async void btn_getLocation_Clicked(object sender, EventArgs e)
         {
             try
@@ -43,6 +43,11 @@ namespace AppTempoAtualTabelado
                     Debug.WriteLine("---------------------------------------");
                 }
 
+                //RETORNA UM POSSIVEL VALOR DE CIDADE A PARTIR DA LONGITUDE A LATITUDE
+                await GetGeocodeReverseData(latitude_value, longitude_vlaue);
+
+                lst_locations.ItemsSource = list_locations_cs;
+
             }
             catch (FeatureNotSupportedException fnsEx)
             {
@@ -61,6 +66,93 @@ namespace AppTempoAtualTabelado
             catch (Exception ex)
             {
                 await DisplayAlert("Erro: ", ex.Message, "OK");
+            }
+        }
+
+        //DECIFRAR A LOCALIZAÇÃO
+          private async Task<string> GetGeocodeReverseData(latitude, longitude)
+        {
+            IEnumerable<Placemark> placemarks = 
+                await Geocoding.Default.GetPlacemarksAsync(
+                    latitude, longitude);
+
+            Placemark? placemark = placemarks?.FirstOrDefault();
+
+            Debug.WriteLine("-------------------------------------------");
+            Debug.WriteLine(placemark?.Locality);
+            Debug.WriteLine("-------------------------------------------");
+
+            if (placemark != null)
+            {
+                cidade = placemark.Locality;
+
+                return
+                    $"AdminArea:       {placemark.AdminArea}\n" +
+                    $"CountryCode:     {placemark.CountryCode}\n" +
+                    $"CountryName:     {placemark.CountryName}\n" +
+                    $"FeatureName:     {placemark.FeatureName}\n" +
+                    $"Locality:        {placemark.Locality}\n" +
+                    $"PostalCode:      {placemark.PostalCode}\n" +
+                    $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                    $"SubLocality:     {placemark.SubLocality}\n" +
+                    $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                    $"Thoroughfare:    {placemark.Thoroughfare}\n";
+            }
+
+            return "Nada";
+        }
+
+        private async void GetWeather()
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(cidade))
+                {
+                    CondicoesClima? previsao = await DataService.GetPrevisaoDoTempo(cidade);
+
+                    string dados_previsao = "";
+
+                    if (previsao != null)
+                    {
+                        dados_previsao = $"Humidade: {previsao.Humidity} \n" +
+                                         $"Nascer do Sol: {previsao.Sunrise} \n " +
+                                         $"Pôr do Sol: {previsao.Sunset} \n" +
+                                         $"Temperatura: {previsao.Temperature} \n" +
+                                         $"Titulo: {previsao.Title} \n" +
+                                         $"Visibilidade: {previsao.Visibility} \n" +
+                                         $"Vento: {previsao.Wind} \n" +
+                                         $"Previsão: {previsao.Weather} \n" +
+                                         $"Descrição: {previsao.WeatherDescription} \n" +
+                                         $"Latitude: {previsao.Latitude} \n" +
+                                         $"Longitude: {previsao.Longitude}";
+
+
+
+                        string url_mapa = $"https://embed.windy.com/embed.html" +
+                                           $"?type=map&location=coordinates&metricRain=mm" +
+                                           $"&metricTemp=°C&metricWind=km/h&zoom=5&overlay=wind" +
+                                           $"&product=ecmwf&level=surface" +
+                                           $"&lat={previsao.Latitude}&lon={previsao.Longitude}";
+
+
+                        Debug.WriteLine(url_mapa);
+                        mapa.Source = url_mapa;                                        
+                    }
+                    else
+                    {
+                        dados_previsao = $"Sem dados, previsão nula.";
+                    }
+
+                    Debug.WriteLine("-------------------------------------------");
+                    Debug.WriteLine(dados_previsao);
+                    Debug.WriteLine("-------------------------------------------");
+
+                    lbl_previsao.Text = dados_previsao;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro ", ex.Message, "OK");
             }
         }
     }
--- AppTempoAtualTabelado/Services/DataServiceCep.cs
+++ AppTempoAtualTabelado/Services/DataServiceCep.cs
@@ -10,31 +10,62 @@ namespace AppTempoAtualTabelado.Services
 {
     class DataServiceCep
     {
-        public static async Task<DadosEndereco?> GetEnderecoByCep(string cep)
+       ublic static async Task<Tempo?> GetPrevisaoDoTempo(string cidade)
         {
-            DadosEndereco? end;
+            // https://openweathermap.org/current#current_JSON
+
+            // https://home.openweathermap.org/api_keys
+            string appId = "6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"http://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={appId}";
+
+            Tempo tempo = null;
 
             using (HttpClient client = new HttpClient())
             {
-                HttpResponseMessage response = await client.GetAsync(
-                    "http://localhost:8000/endereco/by-cep?cep=" + cep);
+                HttpResponseMessage response = await client.GetAsync(url);
 
                 if (response.IsSuccessStatusCode)
                 {
                     string json = response.Content.ReadAsStringAsync().Result;
-                    end = JsonConvert.DeserializeObject<DadosEndereco?>(json);
-                }
-                else
-                {
-                    throw new Exception(response.RequestMessage.Content.ToString());
-                }
 
+                    Debug.WriteLine("----------------------------");
+                    Debug.WriteLine(json);
+                    Debug.WriteLine("----------------------------");
 
+                    //var rascunho = JsonConvert.DeserializeObject(json);
+                    var rascunho = JObject.Parse(json);
 
-            }
+                    Debug.WriteLine("------------------------------------");
+                    Debug.WriteLine(rascunho);
+                    Debug.WriteLine("------------------------------------");
 
-            return end;
+                    DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
 
-        }
+                    // Ajustar de UTC para GMT
+                    // https://stackoverflow.com/questions/6239976/how-to-set-a-time-zone-or-a-kind-of-a-datetime-value
+
+                    tempo = new ()
+                    {
+                        Humidity = (string)rascunho["main"]["humidity"],
+                        Temperature = (string)rascunho["main"]["temp"],
+                        Title = (string)rascunho["name"],
+                        Visibility = (string)rascunho["visibility"],
+                        Wind = (string)rascunho["wind"]["speed"],
+                        Sunrise = sunrise.ToString(),
+                        Sunset = sunset.ToString(),
+                        Weather = (string)rascunho["weather"][0]["main"],
+                        WeatherDescription = (string)rascunho["weather"][0]["description"],
+                        Latitude = (string)rascunho["coord"]["lat"],
+                        Longitude = (string)rascunho["coord"]["lon"],
+                    };                  
+                } 
+            } 
+
+            return tempo;
+        } 
     }
 }
